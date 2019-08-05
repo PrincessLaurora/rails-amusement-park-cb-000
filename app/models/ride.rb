0cf60e23a66efd58201e3587848928bf25d15562
Laura Felce <crepu83@yahoo.it>
2019-08-05 13:51:43 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -5,30 +5,47 @@ class Ride < ActiveRecord::Base
   #validates_presence_of :user_id, :attraction_id
 
   def take_ride
-
-    if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
-      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
-    elsif self.user.tickets < self.attraction.tickets
-      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
-    elsif self.user.height < self.attraction.min_height
-      "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+    enough_tickets, tall_enough = meet_requirements
+    if enough_tickets && tall_enough
+      start_ride
+    elsif tall_enough && !enough_tickets
+      "Sorry. " + ticket_issue
+    elsif enough_tickets && !tall_enough
+      "Sorry. " + height_issue
     else
-      self.user.update(:tickets => new_tickets, :nausea => new_nausea, :happiness => new_happiness)
-      "Thanks for riding the #{self.attraction.name}!"
+      "Sorry. " + ticket_issue + " " + height_issue
     end
   end
 
-  private
-
-    def new_tickets
-      self.user.tickets -= self.attraction.tickets
+  def meet_requirements
+    enough_tickets, tall_enough = false
+    if self.user.tickets >= self.attraction.tickets
+      enough_tickets = true
     end
-
-    def new_nausea
-      self.user.nausea += self.attraction.nausea_rating
+    if self.user.height >= self.attraction.min_height
+      tall_enough = true
     end
+    return [enough_tickets, tall_enough]
+  end
 
-    def new_happiness
-      self.user.happiness += self.attraction.happiness_rating
-    end
+  def start_ride
+    new_happiness = self.user.happiness + self.attraction.happiness_rating
+    new_nausea = self.user.nausea + self.attraction.nausea_rating
+    new_tickets =  self.user.tickets - self.attraction.tickets
+    self.user.update(
+      :happiness => new_happiness,
+      :nausea => new_nausea,
+      :tickets => new_tickets
+    )
+    "Thanks for riding the #{self.attraction.name}!"
+  end
+
+  def ticket_issue
+    "You do not have enough tickets the #{self.attraction.name}."
+  end
+
+  def height_issue
+    "You are not tall enough to ride the #{self.attraction.name}."
+  end
+
 end
